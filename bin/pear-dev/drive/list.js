a13e171b74ec2ea01855d4da4b88461c983ec80d
Kasper Isager Dalsgarð <kasperisager@hey.com>
2023-02-21 14:00:27 +0100

    Add `--mount` option to `drive list`

--- bin/pear-dev/drive/list.js
+++ bin/pear-dev/drive/list.js
@@ -1,10 +1,13 @@
-const { createCommand, createArgument } = require('commander')
+const { createCommand, createArgument, createOption } = require('commander')
 
 module.exports = createCommand('list')
   .description('list the entries of a drive')
   .alias('ls')
   .addArgument(
     createArgument('drive')
+  )
+  .addOption(
+    createOption('-m, --mount <path>', 'mount the entries at a path')
   )
   .action(action)
 
