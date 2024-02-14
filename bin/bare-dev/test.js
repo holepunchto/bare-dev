041d6404d403d7767b11c1d62870fd761b926d5a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-02-14 19:09:50 +0100

    Improve `test` command

--- bin/bare-dev/test.js
+++ bin/bare-dev/test.js
@@ -6,6 +6,15 @@ module.exports = createCommand('test')
     createOption('-b, --build  <path>', 'the path to the build tree')
       .default('build')
   )
+  .addOption(
+    createOption('--timeout <seconds>', 'the default test timeout')
+      .default(30)
+      .argParser((value) => parseInt(value, 10))
+  )
+  .addOption(
+    createOption('-d, --debug', 'configure a debug build')
+      .default(false)
+  )
   .action(action)
 
 function action (_, cmd) {
